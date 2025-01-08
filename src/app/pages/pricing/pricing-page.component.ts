d09f0434f6a86ba336273807963669afa78449a0
Rojas Gomez <sdrojas@skandia.com.co>
2025-01-08 09:05:06 -0500

    Primer commit antes de aplicar el SSR

---
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  styleUrl: './pricing-page.component.css'
+})
+export default class PricingPageComponent {
+
+}
